e6e175787b24b61b9af257a345891d6cfe3e1bb4
jgbalves <jgbalves.23@gmail.com>
2020-10-14 17:17:36 -0300

    'commiting from vscode'

--- Point-mass/SM_coordinates.py
+++ Point-mass/SM_coordinates.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv("C:\\Users\\jgbal\\Github\\lap-time-simulator\\Point-mass\\track_coordinates\\Sample_track.csv", sep = ';', low_memory = False)
 
-df['New column'] = 'default'
+df['New column'] = 'teste'
 
 
 
