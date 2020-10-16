041cec862cefed2de4cbb3744a01234c0cb88b65
jgbalves <jgbalves.23@gmail.com>
2020-10-16 10:00:36 -0300

    small geometry increments

--- Point-mass/SM_coordinates.py
+++ Point-mass/SM_coordinates.py
@@ -1,9 +1,33 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("C:\\Users\\jgbal\\Github\\lap-time-simulator\\Point-mass\\track_coordinates\\Sample_track.csv", sep = ';', low_memory = False)
 
-df['New column'] = 'teste'
+# To add new columns df['New column'] = 'teste'
 
 
 
-print(df.head())
+# print(df.head())
+
+# to find a row (string) print(df.iloc[1])
+
+# Positions 
+# x_pos
+# y_pos
+
+a_two = np.sqrt(
+    (int(df.iat[2,0]) - int(df.iat[0,0]))**2 + 
+    (int(df.iat[2,1]) - int(df.iat[0,1]))**2
+    )
+
+b_two = np.sqrt(
+    (int(df.iat[2,0]) - int(df.iat[1,0]))**2 + 
+    (int(df.iat[2,1]) - int(df.iat[1,1]))**2
+    )
+
+c_two = np.sqrt(
+    (int(df.iat[1,0]) - int(df.iat[0,0]))**2 + 
+    (int(df.iat[1,1]) - int(df.iat[0,1]))**2
+    )
+
+print(c_two)
