de14dace3d7a4f8aa07d54a32bf9a79a662cded2
jgbalves <jgbalves.23@gmail.com>
2020-11-16 09:52:10 -0300

    starting speed columns

--- Point-mass/SM_carmodel.py
+++ Point-mass/SM_carmodel.py
@@ -16,23 +16,36 @@ df['cy'] = pd.to_numeric(df['cy'], downcast= 'float')
 df['Corner Radius'] = pd.to_numeric(df['Corner Radius'], downcast= 'float')
 g_lat = pd.to_numeric(df2.iloc[2,1], downcast='float')
 
-# Creating the column where the corner speeds will be put
-df['speed turn 1'] = np.nan
+
 
 # Giving the columns variable names to simplify
 cx = df['cx']
 cy = df['cy']
 tr = df['Corner Radius']
-cs = df['speed turn 1']
+
+# Converting Radiuses in arrays so they can be used in local minima function
+tr = np.array(tr)
+# finding local minima (turn apex)
+K = np.r_[True, tr[1:] < tr[:-1]] & np.r_[tr[:-1] < tr[1:], True]
+
+# where are the apexes(indexes)
+apexes = [i for i, x in enumerate(K) if x]
+
+for t in apexes:
+    df[f'Test {t}'] = np.nan
+    cs = df[f'Test {t}']
+
+    start = t
+
+    spd_apex = np.sqrt(
+        g_lat * df.at[start, 'Corner Radius'] * 9.81
+    )
 
 # Simple corner velocity (centripetal) V = (g_lat * 9,81 * Radius) ^ 1/2 
-# corner_velocity = np.sqrt(g_lat * tr * 9.81)
 
-# In few moments we will need to create x columns for each local minima
-# Converting Radiuses in arrays so they can be used in local minima function
-# z = np.array(df['Corner Radius'])
-# finding local minima
-# K = np.r_[True, z[1:] < z[:-1]] & np.r_[z[:-1] < z[1:], True]
+# Creating the column where the corner speeds will be put
+df['speed turn 1'] = np.nan
+cs = df['speed turn 1']
 
 start = 6
 spd1 = np.sqrt(g_lat * df.at[start, 'Corner Radius'] * 9.81)
@@ -43,8 +56,6 @@ cx = pd.Series(data= cx)
 cy = pd.Series(data= cy)
 tr = pd.Series(data= tr)
 cs = pd.Series(data= cs)
-
-
 
 for index in range (start+1, cx.size):
     cx_bfr = cx.iat[index -1]
