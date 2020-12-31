9f1fa221eed11c302a24d76ffc14da7a78968cd5
jgbalves <jgbalves.23@gmail.com>
2020-12-31 12:39:33 -0300

    still need to change a lot of df names

---
+++ Point-mass/SM_Drag.py
@@ -0,0 +1,69 @@
+import panda as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+# Track path and car path (track must be a straight line)
+track_details_path = (Path.home(), 'Github', 'lap-time-simulator', 'Point-mass', 'track_coordinates', 'calculated_radiuses.csv')
+car_data_path = Path(Path.home(),'Github', 'lap-time-simulator', 'Point-mass', 'car_data.csv')
+
+track_df = pd.read_csv(track_details_path)
+car_df = pd.read_csv(car_data_path)
+
+# Dropping out null value columns to avoid errors
+car_df.dropna(inplace = True)
+
+# Converting dataframe columns to numeric, to avoid errors
+
+#track
+track_df['cx'] = pd.to_numeric(track_df['cx'], downcast= 'float')
+track_df['cy'] = pd.to_numeric(track_df['cy'], downcast= 'float')
+
+#car
+g_lat = pd.to_numeric(df2.iloc[3,1], downcast='float')
+tranny_efc = pd.to_numeric(df2.iloc[5,1], downcast='float')
+Power = pd.to_numeric(df2.iloc[4,1], downcast='float') * 7457 * tranny_efc
+air_density =  pd.to_numeric(df2.iloc[6,1], downcast='float')
+frontal_area = pd.to_numeric(df2.iloc[1,1], downcast='float')
+drag_coef = pd.to_numeric(df2.iloc[2,1], downcast='float')
+car_mass = pd.to_numeric(df2.iloc[0,1], downcast='float')
+
+# Passing columns as variables to simplify
+cx = track_df['cx']
+cy = track_df['cy']
+
+# Converting from array to series
+cx = pd.Series(cx)
+cy = pd.Series(data= cy)
+
+# Counting the distances between each point
+
+dx = []
+
+for index in range(0, cx.size):
+    cx_bfr = cx.iat[index -1]
+    cx_act = cx.iat[index]
+
+    cy_bfr = cy.iat[index -1]
+    cy_act = cy.iat[index]
+
+    dx.append(
+        np.sqrt(
+            (cx_act - cx_bfr)**2 + (cy_act - cy_bfr)**2
+        )
+    )
+
+track_df['dx'] = dx
+dx = track_df['dx']
+
+# Accelerating
+
+track_df['Velocity'] = np.nan
+velocity = track_df['Velocity']
+
+for index in range (0, cx.size):
+    spd_bfr = 
+
+    drag = drag_coef * air_density * spd_bfr ** 2 * frontal_area / 2
+    cs.at[index] = np.sqrt(spd_bfr**2 + 2 * s_distance *((Power/spd_bfr) - drag) / car_mass)
+
