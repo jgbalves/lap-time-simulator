4b43913a6f6bb5e77a37d85495c4e11d774689b2
jgbalves <jgbalves.23@gmail.com>
2021-03-10 00:13:40 -0300

    filter ok. inserting threshold on turn radius

--- Point-mass/SM_curvature.py
+++ Point-mass/SM_curvature.py
@@ -11,6 +11,7 @@ import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
 from scipy import signal
+import numpy as np
 
 
 ## Building the dataframe
@@ -29,19 +30,26 @@ source_df['G Force Lat'] = pd.to_numeric(source_df['G Force Lat'], downcast= 'fl
 source_df['Corr Speed']  = pd.to_numeric(source_df['Corr Speed'], downcast = 'float')
 source_df['Distance']    = pd.to_numeric(source_df['Distance'], downcast = 'float')
 
+g_lat = source_df['G Force Lat']
 g_lat_ms2  = source_df['G Force Lat'] * 9.81
 speed_ms   = source_df['Corr Speed'] / 3.6
 distance_m = source_df['Distance']
 
-## Creating the radius signal
-turn_radius = speed_ms ** 2 / g_lat_ms2
-
-# Filtering the signal
-fs = 50 #Sampling frequency, according to the source_file sampling frequency
-fc = 3 #cutoff frequency, lower values filter more
-w = fc / (fs / 2) #normalized frequency
+# Filtering the lat acceleration
+fs = 50 # Sampling frequency, according to the source_file sampling frequency
+fc = 1 # Cutoff frequency, lower values filter more
+w = fc / (fs / 2) # Normalized frequency
 b, a = signal.butter(5, w, 'low')
-filtered_turn_radius = signal.filtfilt(b, a, turn_radius)
+filtered_g_lat_ms2 = signal.filtfilt(b, a, g_lat_ms2)
+
+## Creating the radius signal
+turn_radius = speed_ms ** 2 / filtered_g_lat_ms2
+
+for i in turn_radius:
+    if turn_radius[i] > 200:
+        turn_radius_norm = np.inf
+    else:
+        turn_radius_norm = np.sqrt(turn_radius ** 2)
 
 ## Output
 # Output dataframe
@@ -52,20 +60,26 @@ output_df = pd.DataFrame(data= {'Distance':distance_m, 'Turn Radius':turn_radius
 output_path = Path(Path.home(),'Github', 'lap-time-simulator', 'Point-mass', 'track_coordinates', 'turn_radius.csv')
 output_df.to_csv(output_path)
 
-
 '''
+
 ## Plots
-fig, ax = plt.subplots(2)
+fig, ax = plt.subplots(3)
 fig.suptitle('Turn Radius Profile')
 
 # first plot
+ax[0].plot(distance_m, g_lat_ms2, 'r', Label = 'raw (m)')
+ax[0].plot(distance_m, filtered_g_lat_ms2, 'b', Label = 'filtered (m)')
+ax[0].set_title('Lat Accel raw vs filtered')
 
-ax[0].plot(distance_m, turn_radius, 'r', Label = 'raw (m)')
-ax[0].plot(distance_m, filtered_turn_radius, 'b', Label = 'filtered (m)')
-ax[0].set_title('Turn Radius / Distance')
+# second plot
+ax[1].plot(distance_m, turn_radius, 'r', Label = 'raw (m)')
+ax[1].set_title('Turn Radius')
+ax[1].set_ylim([-200, 200]) #putting limit on the y axis
 
-#putting limit on the y axis
+# third plot
+ax[2].plot(distance_m, turn_radius_norm, 'r', Label = 'raw (m)')
 
-ax[0].set_ylim([-200, 200]) 
+
+
 
 plt.show()
