231fe02544b1f5becd058afd07a2e28f7b3a946a
jgbalves <jgbalves.23@gmail.com>
2021-04-13 22:02:50 -0300

    included distance on the finished outing

--- Point-mass/car_analysis.py
+++ Point-mass/car_analysis.py
@@ -17,8 +17,13 @@ import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
 
-class Car_outing:
-    def __init__(self) -> None:
+
+class Car_outing():
+    def __init__(self, csv_name):
+        self.csv_name = csv_name
+        car_data_path = Path(Path.home(),'Github', 'lap-time-simulator', 'Point-mass', csv_name)
+        car_df = pd.read_csv(car_data_path)
+        self.speed_kph = car_df['speed (km/h)']
         pass
 
 def print(car_1, car_2, car_3):
