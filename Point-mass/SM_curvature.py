b6f42a8cbdc6fa03d55cdf9583399c37dfd5ea41
jgbalves <jgbalves.23@gmail.com>
2021-09-23 23:01:20 -0300

    Code polishing

--- Point-mass/SM_curvature.py
+++ Point-mass/SM_curvature.py
@@ -1,18 +1,18 @@
-## ==============================================================================
-## 
-##   _____ ____  ________________     __  ___________________  _____________ 
-##  / ___// __ \/ ____/ ____/ __ \   /  |/  / ____/_  __/ __ \/  _/ ____/   |
-##  \__ \/ /_/ / __/ / __/ / / / /  / /|_/ / __/   / / / /_/ // // /   / /| |
-## ___/ / ____/ /___/ /___/ /_/ /  / /  / / /___  / / / _, _// // /___/ ___ |
-##/____/_/   /_____/_____/_____/  /_/  /_/_____/ /_/ /_/ |_/___/\____/_/  |_|
-##                                                                        
-##                           www.speedmetrica.com
-##
-## ==============================================================================
-## Calculating curvature from longitudinal speed and accelerations
+# # ==============================================================================
+# #
+# #   _____ ____  ________________     __  ___________________  _____________
+# #  / ___// __ \/ ____/ ____/ __ \   /  |/  / ____/_  __/ __ \/  _/ ____/   |
+# #  \__ \/ /_/ / __/ / __/ / / / /  / /|_/ / __/   / / / /_/ // // /   / /| |
+# # ___/ / ____/ /___/ /___/ /_/ /  / /  / / /___  / / / _, _// // /___/ ___ |
+# #/____/_/   /_____/_____/_____/  /_/  /_/_____/ /_/ /_/ |_/___/\____/_/  |_|
+# #
+# #                           www.speedmetrica.com
+# #
+# # ==============================================================================
+# # Calculating curvature from longitudinal speed and accelerations
 
 
-## Importing libraries
+# # Importing libraries
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -20,7 +20,7 @@ from scipy import signal
 import numpy as np
 
 
-## Building the dataframe
+# # Building the dataframe
 
 # choosing file
 source_file = Path(Path.home(),'Github', 'lap-time-simulator', 'Point-mass', 'motec_exports', 'Interlagos_Ohira.csv')
@@ -29,7 +29,7 @@ source_df = pd.read_csv(source_file, sep = ',', low_memory = False, skiprows=13)
 # Dropping null value columns out to avoid errors
 source_df.dropna(inplace = True)
 
-## Choosing columns to calculate the necessary values
+# # Choosing columns to calculate the necessary values
 
 # converting dataframe colums to numeric to avoid errors
 source_df['G Force Lat'] = pd.to_numeric(source_df['G Force Lat'], downcast= 'float')
@@ -45,24 +45,24 @@ distance_m = source_df['Distance']
 fs = 50 # Sampling frequency, according to the source_file sampling frequency
 fc = 1 # Cutoff frequency, lower values filter more
 w = fc / (fs / 2) # Normalized frequency
-b, a = signal.butter(5, w, 'low') # Appliying a Butterworth filter
+b, a = signal.butter(5, w, 'low')    # Appliying a Butterworth filter
 filtered_g_lat_ms2 = signal.filtfilt(b, a, g_lat_ms2)
 
-## Creating the radius signal
+# # Creating the radius signal
 turn_radius = speed_ms ** 2 / filtered_g_lat_ms2
 turn_radius_norm = np.sqrt(turn_radius ** 2)
 
-## Output
+# # Output
 # Output dataframe
 output_df = pd.DataFrame(data= {'Distance':distance_m, 'Turn Radius':turn_radius_norm})
 
 
-#exporting it
+# Exporting it
 output_path = Path(Path.home(),'Github', 'lap-time-simulator', 'Point-mass', 'track_coordinates', 'turn_radius.csv')
 output_df.to_csv(output_path)
 
 
-## Plots
+# # Plots
 fig, ax = plt.subplots(3)
 fig.suptitle('Turn Radius Profile')
 
@@ -74,13 +74,13 @@ ax[0].set_title('Lat Accel raw vs filtered')
 # second plot - Turn Radius
 ax[1].plot(distance_m, turn_radius, 'r', Label = 'raw (m)')
 ax[1].set_title('Turn Radius')
-ax[1].set_ylim([-200, 200]) #putting limit on the y axis
+ax[1].set_ylim([-200, 200])    # putting limit on the y axis
 
 # third plot - Curvature
 ax[2].plot(distance_m, turn_radius_norm, 'r', Label = 'raw (m)')
 ax[2].set_title('Curvature')
-ax[2].set_ylim([0, 200]) #putting limit on the y axis
+ax[2].set_ylim([0, 200])    # Putting limit on the y axis
 
-fig.tight_layout(pad = 1.0) #spacing, so plot titles don't go over each other 
+fig.tight_layout(pad = 1.0)    # Spacing, so plot titles don't go over each other
 
 plt.show()
