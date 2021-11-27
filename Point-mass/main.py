9c62dafc382d240106877dc1dc1817ca40735c91
jgbalves <jgbalves.23@gmail.com>
2021-11-26 23:03:23 -0300

    refactoring to oop

--- Point-mass/main.py
+++ Point-mass/main.py
@@ -17,10 +17,12 @@ import SM_car_analysis as ltsplt
 
 
 def main():
+    # To run the car around the track
     # car = lts.Car('car_data_2.csv')
     # track = lts.Track('turn_radius.csv')
     # lts.simulate(car, track)
 
+    # To plot the cars created
     car_1 = ltsplt.CarOuting('car_data_1_turn_radius_outing.csv')
     car_2 = ltsplt.CarOuting('car_data_2_turn_radius_outing.csv')
     car_3 = ltsplt.CarOuting('car_data_3_turn_radius_outing.csv')
